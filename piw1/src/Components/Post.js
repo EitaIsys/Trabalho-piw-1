9dee7d601515752208b623f28f808eeb165a1ba3
Isys <isys-schmitt@hotmail.com>
2020-03-13 23:25:16 -0300

    Commit inicial

--- piw1/src/Components/Post.js
+++ piw1/src/Components/Post.js
@@ -5,7 +5,7 @@ class Post extends React.Component{
     render(){
         return (
             <div className='post'> 
-                <div className='nickname'> Peta </div>
+                <div className='nickname'> Pata </div>
                 <div className='msg'>Algumas pessoas tem talento, outras beleza e eu não tenho nada</div>
                 <div className='nickname'> Peta </div>
                 <div className='msg'>Eu nunca vou perdoar um Ministério da Educação que não pegou o MC Livinho e fez o sósia MC Livrinho: o MC da Educação</div>
@@ -13,7 +13,7 @@ class Post extends React.Component{
                 <div className='msg'>sinceramente não ligo pra beijo na boca eu queria mesmo é namorar pra ter alguém pra ver cocoricó cmg</div>
                 <div className='nickname'> Pota </div>
                 <div className='msg'>Bandido bom é bandido que disponibiliza o jogo completo com tutorial de instalação, crack e tradução.</div>
-                <div className='nickname'> Patrica </div>
+                <div className='nickname'> Larissa </div>
                 <div className='msg'>Aquela perda de dignidade rápida quando você espirra e solta um pum junto</div>
 
             </div>
